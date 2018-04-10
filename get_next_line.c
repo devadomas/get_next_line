9a6c412ac0bbfe8d6ccb1dde436def77a39445af
Adomas ZALIAUSKAS <azaliaus@e2r4p23.42.fr>
2018-04-10 18:58:16 +0200

    version with leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 14:29:52 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/04/10 15:15:54 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/04/10 18:39:08 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@
 static t_reader	*init_reader(int fd)
 {
 	t_reader		*rdr;
-	char c[10];
 
 	rdr = (t_reader *)malloc(sizeof(t_reader));
 	if (!rdr)
 		return (0);
 	rdr->fd = fd;
 	rdr->error = 0;
-	rdr->error = read(fd, &c, 0);
+	rdr->error = 0;
 	rdr->line = NULL;
 	rdr->next = NULL;
 	return (rdr);
@@ -46,10 +45,8 @@ static t_reader	*load_reader(t_reader **reader, int fd)
 		while (cpy)
 		{
 			if(cpy->fd == fd)
-			{
 				return (cpy);
-			}
-			if (cpy->next)
+			if (!cpy->next)
 				break ;
 			cpy = cpy->next;
 		}
@@ -64,43 +61,28 @@ static int		read_line(t_reader *reader, char **line)
 	int		cut_len;
 	int		read_ret;
 	char	buff[BUFF_SIZE + 1];
-	char	*tmp;
 
-	read_ret = -1;
-	if (!reader->line || !strchr(reader->line, '\0')) /* loading more if needed */
+	if (!reader->line || !ft_strchr(reader->line, '\n'))
 	{
-		while (((read_ret = read(reader->fd, &buff, BUFF_SIZE)) > 0)
-				|| !strchr(reader->line, '\n'))
+		while ((read_ret = read(reader->fd, &buff, BUFF_SIZE)) > 0)
 		{
-			if (read_ret == 0)
-				break ;
 			buff[read_ret] = '\0';
-			if (!reader->line)
-				reader->line = ft_strdup(buff);
-			else
-				reader->line = ft_strjoin(reader->line, buff);
+			reader->line = ft_strjoin(reader->line, buff);
+			if (ft_strchr(reader->line, '\n'))
+				break ;
 		}
-		if (read_ret < 0)
-			return (-1);
-		if (read_ret == 0 && !reader->line)
-			return (0);
+		if (read_ret < 0 || (read_ret == 0 && !reader->line))
+			return (read_ret);
 	}
-	/* after loading more */
+	if (!(reader->line))
+		return (-1);
 	if (ft_strlen(reader->line) == 0)
 		return (0);
-	tmp = ft_get_next_word(reader->line, '\n');
-	*line = tmp;
-	cut_len = ft_strlen(tmp) + (ft_strchr(reader->line, '\n')? 1 : 0);
-	//printf("Counter: %d\n",(int)ft_strlen(reader->line) - cut_len);
-	if (ft_strlen(reader->line) == ft_strlen(*line))
-		reader->line = ft_strdup("");
-	else
-		reader->line = ft_strsub(reader->line, cut_len,
+	*line = ft_get_next_word(reader->line, '\n');
+	cut_len = ft_strlen(*line) + (ft_strchr(reader->line, '\n')? 1 : 0);
+	reader->line = ft_strsub(reader->line, cut_len,
 			(int)ft_strlen(reader->line) - cut_len);
-	
-	//printf("-------------------------\n");
-	return (ft_strlen(*line));
-	//return (0);
+	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
